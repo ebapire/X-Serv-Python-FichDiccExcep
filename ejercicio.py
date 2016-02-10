b5649a6461d81a09b92852a8757bd59017dbb7dc
ebapire <eva.garcia.domingo@gmail.com>
2016-02-10 14:19:33 +0100

    terminado

--- ejercicio.py
+++ ejercicio.py
@@ -8,6 +8,13 @@ fd = open('/etc/passwd', 'r')
 lineas = fd.readlines()
 fd.close()
 
+lista_usuarios = {}
 for linea in lineas:
     elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
+    lista_usuarios[elementos[0]] = elementos[-1][:-1]
+
+print "root: " + lista_usuarios['root']
+try:
+    print "Imaginario: " + lista_usuarios['imaginario']
+except KeyError:
+    print "usuario incorrecto"
